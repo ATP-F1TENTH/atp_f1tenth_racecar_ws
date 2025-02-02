eb31c3d6722946cbbe7a5babcc7025df209ee4bd
salkin898 <dohm.niklas@outlook.de>
2025-02-02 13:19:34 +0100

    Updated build-script / submodule raceline.

--- compile.sh
+++ compile.sh
@@ -41,6 +41,11 @@ then
     SESSION=createMap;
     CMD1="ros2 launch vehicle_control ${launch_file}";
     screen -dmS ${SESSION} bash -c "${CMD1} ; ${HOLD}";
+    launch_node="mapping_localization/localization_launch_amcl.py";
+    file="mapping_localization/nav2_params.yaml";
+    map="/home/itse/atp_f1tenth_racecar_ws/src/f1tenth_gym_ros/maps/levine_blocked.yaml";
+    CMD2="ros2 launch ${launch_node} params_file:=${file} map:=${map}";
+    screen -S $SESSION -X screen bash -c "${CMD2} ; ${HOLD}";
     screen -r $SESSION;
 else
     ros2 launch vehicle_control ${launch_file}
